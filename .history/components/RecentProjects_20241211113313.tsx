b8ef450dd1595b69eefe9dc715affab7c4bee3b0
Joseph <joseph8778@gmail.com>
2024-12-11 12:38:37 -0600

    added projects section

---
+++ .history/components/RecentProjects_20241211113313.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecentProjects = () => {
+    return (
+        <div className='py-20'>
+            <h1 className="heading"></h1>
+        </div>
+    );
+}
+
+export default RecentProjects;
---
+++ .history/components/RecentProjects_20241211113829.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RecentProjects = () => {
+    return (
+        <div className='py-20'>
+            <h1 className="heading">
+                A small selection of {' '}
+                <span className='text-purple'>recent projects.</span>
+            </h1>
+            <div className=''>
+
+            </div>
+        </div>
+    );
+}
+
+export default RecentProjects;
